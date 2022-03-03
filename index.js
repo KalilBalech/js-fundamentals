ab2bf1f75449f0959a9062251e26b3251f07c0fa
Kalil Georges Balech <kalilgb152@gmail.com>
2022-03-03 14:01:54 -0300

    third page done

--- index.js
+++ index.js
@@ -220,4 +220,59 @@ exemplos abaixo para um melhor entendimento:
 
 */
 
-function objetoParaArray
+function objetoParaArray(objeto){
+    let array = Object.entries(objeto)
+    return array
+}
+
+/*
+
+Elabore uma função que receba um array de números e retorne um array que tenha todos os números que são
+pares e que também tenham índices pares
+
+*/
+
+function receberSomenteOsParesDeIndicePar(array){
+    
+    let outroArray=[]
+
+    array.forEach((e, index) =>{
+        if(e%2===0 && index%2===0){
+            outroArray.push(e)
+        }
+    })
+}
+
+/*
+
+A fim de manter o calendário anual ajustado com o movimento de translação da Terra, criou-se os anos
+bissextos, que têm 366 dias em vez dos 365 presentes nos anos normais.
+Para determinar se um ano é bissexto, é necessário saber se ele é multiplo de 4. Não pode ser múltiplo de 100,
+exceto se for também múltiplo de 400.
+Com isso em mente, desenvolva uma função que recebe um número correspondente a um ano e retorna se ele
+é bissexto ou não
+
+*/
+
+function checarAnoBissexto(ano){
+
+    if(ano%4===0){
+
+        if(ano%100 ===0){
+
+            if(ano%400 === 0){
+                return true
+            }
+            else{
+                return false
+            }
+            
+        }
+        else{
+            return true
+        }
+    }
+    else{
+        return false
+    }
+}
