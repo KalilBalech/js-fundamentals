991d02830920eca319e6ffb538b2b1b830495deb
Kalil Georges Balech <kalilgb152@gmail.com>
2022-03-03 13:30:18 -0300

    second page done

--- index.js
+++ index.js
@@ -76,3 +76,148 @@ function inverso(a){
         return frase
     }
 }
+
+/*
+Crie uma função que receba quatro números como parâmetro (numero, minimo, maximo, inclusivo) e retorne se
+o parâmetro numero (o primeiro) está entre minimo e maximo. Quando o parâmetro inclusivo for true, tenha
+"entre" como inlusivo, ou seja, considerando se numero é igual a minimo ou a maximo. Caso o parâmetro
+inclusivo não seja informado, seu valor padrão deverá ser false, portanto, a lógica será exclusiva, não
+considerando se numero é igual a minimo ou a maximo.
+*/
+
+function estaEntre (numero, a, b, inclusivo=false){
+    
+    if(a>=b){
+        let maior = a
+        let menor = b
+    }
+    else{
+        let maior = b
+        let menor = a
+    }
+    
+    if((inclusivo && numero>=menor && numero<=maior) || (!inclusivo && numero>menor && numero<maior)){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+/*
+Desenvolva uma função que recebe dois números inteiros não negativos (maiores ou iguais a zero) e realize a
+multiplicação deles. Porém, não utilize o operador de mutiplicação
+*/
+
+function multiplicar(a, b){
+    let resultado = 0
+    let i
+
+    for(i=0; i<b; i++){
+        resultado +=a
+    }
+
+    return resultado
+}
+
+/*
+Escreva uma função que receba dois parâmetros. O primeiro parâmetro é o elemento que repetirá, enquanto
+que o segundo será o número de vezes que haverá repetição. Um array será retornado
+*/
+
+function repetir(elemento, numeroDeRepeticoes){
+
+    let lista=[]
+
+    let i
+
+    for(i=0; i<numeroDeRepeticoes; i++){
+        lista.push(elemento)
+    }
+
+    return lista
+}
+
+/*
+Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na
+quantidade especificada no parâmetro
+*/
+
+function simboloMais(numero){
+    let simbolos = ""
+    let i
+
+    for(i=0; i<numero; i++){
+        simbolos+="+"
+    }
+
+    return simbolos
+}
+
+/*
+Crie uma função que receba uma array e retorne o primeiro e o último elemento desse array como um novo
+array
+*/
+
+function receberPrimeiroEUltimoElemento(array){
+    let novoarray = []
+    novoarray.push(array[0])
+    novoarray.push(array[-1])
+    return novoarray
+}
+
+/*
+
+Quando temos um objeto e manipulamos seus atributos, adicionando, atualizando ou removendo-os, estamos
+apenas modificando-o, mas, em essência, o objeto continua o mesmo, ou seja a sua referência de memória é a
+mesma.
+Num projeto que você está trabalhando, você foi designado a refatorar diversas funções para que façam
+cópias de objetos e manipulem os dados dessas cópias, com o intuito de evitar efeitos indesejáveis em
+algumas situações devido a referências a objetos. Abaixo, está a descrição de uma dessas funções.
+Você escreverá uma função que recebe um objeto como primeiro parâmetro e, como segundo parâmetro, o
+nome de uma propriedade contida nesse objeto. Em seguida, retorne uma cópia desse objeto sem a
+propriedade especificada no segundo parâmetro.
+
+*/
+
+function removerPropriedade(objeto, caracteristica){
+    let copia = objeto
+    delete copia[caracteristica] // DEVE-SE UTILIZAR A SEGUNDA FORMA DE ACESSAR ATRIBUTOS DE UM OBJETO
+    return copia
+}
+    /*
+    
+    Existem 3 formas de remover uma propriedade de um objeto. A primeira é através do delete, como foi feito. É uma execução 
+    que realmente deleta o atributo, mas tem tempo de execução alto. A segunda forma é definindo o atributo como undefined ou
+    como string nula. As ultimas duas formas não deletam o atributo, mas tem tempo de execução maior.
+    
+    */
+
+/*
+
+Crie uma função que receba um array de elementos e retorne um array somente com os números presentes no
+array recebido como parâmetro.
+
+*/
+
+function filtrarNumero(array){
+    let arrayNumerico = []
+
+    array.forEach(elemento =>{
+        if(typeof(elemento)==="number"){
+            arrayNumerico.push(elemento)
+        }
+    })
+
+    return arrayNumerico
+}
+
+/*
+
+Desenvolva uma função que recebe como parâmetro um objeto e retorne um array de arrays, em que cada
+elemento é um array formado pelos pares chave/valor que corresponde a um atributo do objeto. Observe os
+exemplos abaixo para um melhor entendimento:
+
+*/
+
+function objetoParaArray
